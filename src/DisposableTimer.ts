3ef0243a49391f27eaa41af688768cb0124ad2c4
Joost Lubach <joostlubach@gmail.com>
2023-10-11 14:58:06 +0200

    Interface to more closely resemble setTieout.

--- src/DisposableTimer.ts
+++ src/DisposableTimer.ts
@@ -1,16 +1,9 @@
 import config from './config'
 
-const activeTimers: Set<DisposableTimer> = new Set()
-
 export default class DisposableTimer {
 
-  private disposed = false
-
-  public get isDisposed() {
-    return this.disposed
-  }
-
   private timeouts: Set<NodeJS.Timer> = new Set()
+  private disposed = false
 
   /**
    * Determines if this timer is currently active, i.e. there are active timeouts.
@@ -20,77 +13,90 @@ export default class DisposableTimer {
   }
 
   /**
-   * Queues a task like `setTimeout`, but makes sure this task is canceled if this runner is disposeed.
-   * The given callback is executed with the runner as `this`.
-   *
-   * @param callback The callback to execute.
-   * @param ms       The amount of milliseconds to wait.
-   * @returns        A promise with the return value of the callback. Note that if the runner is disposeed,
-   *                 this promise is never resolved or rejected!
+   * Determines if this timer has been disposed.
    */
-  public setTimeout<T>(callback: () => T | Promise<T>, ms: number): Promise<T> {
-    return new Promise((resolve, reject) => {
-      if (this.disposed) { return }
-
-      const timeout = setTimeout(async () => {
-        this.timeouts.delete(timeout)
-        this.removeIfInactive()
-
-        try {
-          const retval = await callback.call(this)
-          resolve(retval)
-        } catch (error: any) {
-          reject(error)
-        }
-      }, ms)
-
-      this.timeouts.add(timeout)
-      activeTimers.add(this)
-    })
+  public get isDisposed() {
+    return this.disposed
   }
 
-  public clearTimeouts() {
-    if (this.timeouts.size === 0) { return }
+  /**
+   * Wrapper around global setTimeout().
+   */
+  public setTimeout(fn: () => any, ms: number): NodeJS.Timeout | null {
+    if (this.disposed) { return null }
 
+    const timeout = setTimeout(async () => {
+      this.timeouts.delete(timeout)
+      this.removeIfInactive()
+      fn()
+    }, ms)
+
+    this.timeouts.add(timeout)
+    ACTIVE_TIMERS.add(this)
+
+    return timeout
+  }
+
+  /**
+   * Wrapper around global clearTimeout().
+   */
+  public clearTimeout(timeout: NodeJS.Timeout) {
+    clearTimeout(timeout)
+    this.timeouts.delete(timeout)
+    this.removeIfInactive()
+  }
+
+  /**
+   * Clears all timeouts.
+   */
+  public clearAll() {
+    if (this.timeouts.size === 0) { return }
     for (const timeout of this.timeouts) {
       clearTimeout(timeout)
     }
     this.timeouts.clear()
-
-    activeTimers.delete(this)
+    ACTIVE_TIMERS.delete(this)
   }
 
+  /**
+   * Utility Promise based delay function.
+   */
   public delay(ms: number) {
     return new Promise<void>(resolve => {
       this.setTimeout(resolve, ms)
     })
   }
 
-  public removeIfInactive() {
-    if (!this.isActive) {
-      activeTimers.delete(this)
-    }
-  }
+  // #endregion
+
+  // #regoin Housekeeping
 
   /**
    * Kills all outstanding tasks of this runner.
    */
   public dispose() {
-    this.clearTimeouts()
+    this.clearAll()
     this.disposed = true
+  }
+
+  private removeIfInactive() {
+    if (!this.isActive) {
+      ACTIVE_TIMERS.delete(this)
+    }
   }
 
   /**
    * Kills all active runners in the swarm.
    */
   public static disposeAll() {
-    config.logger.debug("Received dispose signal, shutting down all timers...", {
-      count: activeTimers.size,
-    })
-
-    for (const timer of activeTimers) {
+    for (const timer of ACTIVE_TIMERS) {
       timer.dispose()
     }
+    ACTIVE_TIMERS.clear()
   }
 
+  // #endregion
+
 }
+
+const ACTIVE_TIMERS: Set<DisposableTimer> = new Set()
